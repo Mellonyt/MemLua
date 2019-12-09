b3791216726d9ffc65166e2125bad98a3b5dde7d
thedoomed <46077863+thedoomed@users.noreply.github.com>
2019-12-08 21:59:23 -0800

    Create EVM.h

---
+++ EVM.h
@@ -0,0 +1,17 @@
+// Emulated Virtual Machine for ROBLOX!
+// 
+// This relies not on instructions, but roblox's own stack!
+// this method uses 30% of the functions a wrapper would use,
+// yet has the same power and efficiency as a virtual machine!
+// 
+// this way, scripts can be executed with ease
+// at astounding speeds! (who tf cares about speed in roblox)
+// but most of all:
+// 
+// ill be making this open-source along with the self-updating
+// API and foundation that all my code is based on.
+// This means that nothing can be patched!
+// I'll continue to release updates for it unless silly roblox
+// unterminates my OG account, `static`.
+//
+// 
